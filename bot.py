da8c066eabe050d2e87aea1e1c12b8d221b5db48
ultralapse <136134023+ultralapse@users.noreply.github.com>
2024-06-05 09:21:35 -0400

    feat: Print out operating system information.

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import discord
 import os
+import platform
 from discord.ext import commands
+from discord import app_commands
 
 intents = discord.Intents.all()
-intents.messages = True
 client = commands.Bot(command_prefix = '!', intents=intents)
 
 @client.event
@@ -14,6 +15,16 @@ async def on_ready():
 @client.command()
 async def ping(ctx):
     await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
+
+@client.command()
+async def info(ctx):
+    os_info = platform.uname()
+    await ctx.send(f"Operating System: {os_info.system}\n"
+                   f"Release: {os_info.release}\n"
+                   f"Version: {os_info.version}\n"
+                   f"Machine: {os_info.machine}\n"
+                   f"Processor: {os_info.processor}")
+
 
 
 client.run(os.getenv('BOT_TOKEN'))
