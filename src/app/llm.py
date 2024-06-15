6de8dbe7a68aa92e29027d2e9160935c199b5f91
ultralapse <136134023+ultralapse@users.noreply.github.com>
2024-06-14 20:31:20 -0400

    chore!: refactor LLM logic for better code readability

--- src/app/llm.py
+++ src/app/llm.py
@@ -1,5 +1,6 @@
 from umlnow import course, Search, API
 import re
+import course
 import boto3
 import json
 import os
@@ -9,7 +10,25 @@ BEDROCK_KEY = os.getenv("BEDROCK_KEY")
 KNOWLEDGE = os.getenv("KNOWLEDGE_BASE_ID")
 
 def invoke_llm(input):
-    list = course_process(input)
+    isCourse = LLMTitanLite(input + isCourseAugment(input))
+
+    if isCourse == "yes":
+        # Regex that filters out course IDs
+        list = course_process(input)
+
+        # Decide what user is asking about for courses
+        outputText = LLMTitanLite(UMLNowAugment(input))
+
+        match outputText:
+            case "prerequisites": return course.course_info("prereq", list[0])
+            case "name": return course.course_info("name", list[0])
+            case "credits": return course.course_info("credits", list[0])
+
+    else:
+        return LLMTitanPremier(RAGTemplate(input))
+
+# Call The Titan Lite Model (No RAG Capabilities, only for decision making and scraping UML Now)
+def LLMTitanLite(input):
     bedrock = boto3.client(
         service_name='bedrock-runtime', 
         region_name='us-east-1',
@@ -18,13 +37,11 @@ def invoke_llm(input):
         
     )
 
-    truefalse = input+trueorFalse(input)
-
     modelId = 'amazon.titan-text-lite-v1'
     accept = 'application/json'
     contentType = 'application/json'
     body = json.dumps({
-        "inputText": truefalse,
+        "inputText": input,
         "textGenerationConfig":{
             "maxTokenCount":256,
             "stopSequences":[],
@@ -33,65 +50,35 @@ def invoke_llm(input):
             }
     })
 
-    # Is the user asking about a course?
+ 
     response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
-
     response_body = json.loads(response.get('body').read())
-    outputText = response_body.get('results')[0].get('outputText')
+    return response_body.get('results')[0].get('outputText')
 
-    if outputText == "yes":
-        body = json.dumps({
-            "inputText": template_message(input),
-            "textGenerationConfig":{
-                "maxTokenCount":256,
-                "stopSequences":[],
-                "temperature":0,
-                "topP":0.9
-                }
-        })
-        response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
-        response_body = json.loads(response.get('body').read())
-        outputText = response_body.get('results')[0].get('outputText')
-
-        if outputText == "prerequisites":
-            return course_info("prereq", list[0])
-        elif outputText == "name":
-            return course_info("name", list[0])
-        elif outputText == "credits":
-            return course_info("credits", list[0])
-
-    else:
-        bedrock = boto3.client(
-            service_name='bedrock-agent-runtime', 
-            region_name='us-east-1',
-            aws_access_key_id=BEDROCK_ID,
-            aws_secret_access_key=BEDROCK_KEY
+# Call the Titan Premier Model (RAG Capabilities)
+def LLMTitanPremier(input):
+    bedrock = boto3.client(
+        service_name='bedrock-agent-runtime', 
+        region_name='us-east-1',
+        aws_access_key_id=BEDROCK_ID,
+        aws_secret_access_key=BEDROCK_KEY
             
-        )   
-        return bedrock.retrieve_and_generate(
-            input={
-                'text': RAGTemplate(input)
-            },
-            retrieveAndGenerateConfiguration={
-                'type': 'KNOWLEDGE_BASE',
-                'knowledgeBaseConfiguration': {
-                    'knowledgeBaseId': KNOWLEDGE,
-                    'modelArn': 'arn:aws:bedrock:us-east-1::foundation-model/amazon.titan-text-premier-v1:0'
-                    }
+    )   
+    return bedrock.retrieve_and_generate(
+        input={
+            'text': input
+        },
+        retrieveAndGenerateConfiguration={
+            'type': 'KNOWLEDGE_BASE',
+            'knowledgeBaseConfiguration': {
+                'knowledgeBaseId': KNOWLEDGE,
+                'modelArn': 'arn:aws:bedrock:us-east-1::foundation-model/amazon.titan-text-premier-v1:0'
                 }
-            )["output"]["text"]
+            }
+        )["output"]["text"]
 
-
-    return outputText
-
-
-def refine_out(message):
-    prompt = '''\n
-    Refine this output to make it more human readable
-    '''
-    return message + prompt
-
-def template_message(message):
+# Augmented Prompts
+def UMLNowAugment(message):
 
     prompt_template3 = '''\n
     What is the user asking about for the course?? Choose from the following:
@@ -99,37 +86,20 @@ def template_message(message):
     '''
     return message + prompt_template3
 
-def trueorFalse(message):
+def isCourseAugment(message):
     prompt_template = '''\n
     Is the user asking about a course? Choose from the following:
     yes, no.
     '''
     return message + prompt_template
 
+def RAGTemplate(message):
+    prompt_template = '''\n
+    You are a chatbot for the University of Massachusetts Lowell. Answer the question as if you a tour guide.
+    '''
+    return message + prompt_template
 
-
-def course_info(option, course_id):
-    if option == "name":
-        message_content = getCourse(course_id)
-    elif option == "prereq":
-        message_content = getPre(course_id)
-    elif option == "credits":
-        message_content = getCr(course_id)
-    else:
-        message_content = "Incorrect option"
-    
-    return message_content
-
-def getCourse(courseID):
-    return course.get_course_name(course.get_html_response(course.get_course_url(courseID)))
-
-def getPre(courseID):
-    return course.get_course_requirements_text(course.get_html_response(course.get_course_url(courseID)))
-
-def getCr(courseID):
-    dict = course.get_course_credits(course.get_html_response(course.get_course_url(courseID)))
-    return f"Credits: {dict['min']}"
-
+# Regex Matcher
 def course_process(message):
     course_matches = []
 
@@ -145,74 +115,6 @@ def course_process(message):
 
     return course_matches
 
-def decisionTree(message):
-
-    bedrock = boto3.client(
-        service_name='bedrock-runtime', 
-        region_name='us-east-1',
-        aws_access_key_id=BEDROCK_ID,
-        aws_secret_access_key=BEDROCK_KEY
-            
-    )
-
-    modelId = 'amazon.titan-text-premier-v1:0'
-    accept = 'application/json'
-    contentType = 'application/json'
-    body = json.dumps({
-        "inputText": message,
-        "textGenerationConfig":{
-            "maxTokenCount":256,
-            "stopSequences":[],
-            "temperature":0,
-            "topP":0.9
-            }
-    })
-
-    response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
-
-    response_body = json.loads(response.get('body').read())
-    outputText = response_body.get('results')[0].get('outputText')
-    print(outputText)
-    return outputText
-
-
-def retrieveAndGenerate(input, kbId):
-    bedrock = boto3.client(
-            service_name='bedrock-agent-runtime', 
-            region_name='us-east-1',
-            aws_access_key_id=BEDROCK_ID,
-            aws_secret_access_key=BEDROCK_KEY
-            
-    )
-
-    if decisionTree(housing(input)) == "yes":
-        
-
-        return bedrock.retrieve_and_generate(
-            input={
-                'text': RAGTemplate(input)
-            },
-            retrieveAndGenerateConfiguration={
-                'type': 'KNOWLEDGE_BASE',
-                'knowledgeBaseConfiguration': {
-                    'knowledgeBaseId': kbId,
-                    'modelArn': 'arn:aws:bedrock:us-east-1::foundation-model/amazon.titan-text-premier-v1:0'
-                    }
-                }
-            )["output"]["text"]
-
-def housing(message):
-    prompt_template = '''\n
-    Is this statement asking about housing or living somewhere? Choose from the following:
-    yes, no.
-    '''
-    return message + prompt_template
-
-def RAGTemplate(message):
-    prompt_template = '''\n
-    You are a chatbot for the University of Massachusetts Lowell. Answer the question as if you a tour guide.
-    '''
-    return message + prompt_template
 
 
 
