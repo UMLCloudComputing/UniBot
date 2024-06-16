26765b0672ea9d10ba9f1d35517b673a6b9351db
ultralapse <136134023+ultralapse@users.noreply.github.com>
2024-06-16 17:14:47 -0400

    feat: experiment to improve accuracy of housing information

--- src/app/llm.py
+++ src/app/llm.py
@@ -100,6 +100,11 @@ def isCourseAugment(message):
 def RAGTemplate(message):
     prompt_template = '''\n
     You are a chatbot for the University of Massachusetts Lowell. Answer the question as if you a tour guide.
+
+    When the user asks about housing, you should provide information about the residence hall, and its price.
+    When the user asks about what is the cost of x, always provide the price per academic year unless the user asks otherwise.
+    
+    You may be tempted to say "I don't know" or "Please clarify your question" when you don't know something. However, you should avoid doing so. Instead, you should provide a response that is as helpful as possible.
     '''
     return message + prompt_template
 
