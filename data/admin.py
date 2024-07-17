0ed1cde35df123b2558b5c822cb8744b330acd6d
ultralapse <136134023+ultralapse@users.noreply.github.com>
2024-07-16 20:10:28 -0400

    chore!: Having a separate bucket to hold metadata is redundant

--- data/admin.py
+++ data/admin.py
@@ -31,27 +31,29 @@ def load_from_database(dataBucket, metadataBucket):
         update_bucket(list_item, dataBucket, metadataBucket)
     
 
-def download_html_and_create_json(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    html_content = soup
+# def download_html_and_create_json(url):
+#     response = requests.get(url)
+#     soup = BeautifulSoup(response.text, 'html.parser')
+#     html_content = soup
 
-    parsed_url = urlparse(url)
-    filename = re.sub(r'\W+', '_', parsed_url.netloc + parsed_url.path) + ".html"
+#     parsed_url = urlparse(url)
+#     filename = re.sub(r'\W+', '_', parsed_url.netloc + parsed_url.path) + ".html"
 
-    with open(filename, 'w') as file:
-        file.write(html_content)
+#     with open(filename, 'w') as file:
+#         file.write(html_content)
 
-    url_data = {"urls": url}
+#     url_data = {"urls": url}
 
-    with open(filename + '.json', 'w') as json_file:
-        json.dump(url_data, json_file)
+#     with open(filename + '.json', 'w') as json_file:
+#         json.dump(url_data, json_file)
     
-    return filename
+#     return filename
 
-def update_bucket(url, bucket_name, source_bucket):
+def update_bucket(url, bucket_name):
     s3 = boto3.client('s3', aws_access_key_id=S3_ID, aws_secret_access_key=S3_KEY)
     
+    print("Succesfully created S3 Bucket")
+
     response = requests.get(url)
     html_content = response.content
 
@@ -63,13 +65,18 @@ def update_bucket(url, bucket_name, source_bucket):
     url_data = {"url": url}
     json_content = json.dumps(url_data).encode('utf-8')  # Convert to bytes
 
-    s3.put_object(Bucket=source_bucket, Key=filename + '.json', Body=BytesIO(json_content))
+    s3.put_object(Bucket=bucket_name, Key=filename + '.json', Body=BytesIO(json_content))
 
     return filename
 
-st.title('Knowledge Base Tools')
+st.title('S3 Tools')
 
 url = st.text_input('Enter the URL')
+bucket = st.text_input('Enter the bucket name')
+source = st.text_input('Enter the metadata bucket name')
+
+if st.button("Add URL directly to S3 Bucket"):
+    update_bucket(url, bucket)
 
 if st.button("Add URL to database"):
     with open('urls.json', 'r') as file:
@@ -81,8 +88,7 @@ if st.button("Add URL to database"):
     
     st.success('URL added to database.')
 
-bucket = st.text_input('Enter the bucket name')
-source = st.text_input('Enter the metadata bucket name')
+
 
 with open('urls.json', 'r') as file:
     data = json.load(file)
@@ -96,6 +102,9 @@ with open('urls.json', 'r') as file:
 
 if st.button("Load from Database"):
     load_from_database(bucket, source)
+
+
+st.title("Knowledge Base Ingestion")
 
 knowledge_base = st.text_input('Enter the knowledge base name')
 knowledge_source = st.text_input('Enter the knowledge base source ID')
