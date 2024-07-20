4f7deb6ad60b62be2822a319f45c384f01243855
ultralapse <136134023+ultralapse@users.noreply.github.com>
2024-07-19 20:01:03 -0400

    feat!: update bedrock agents for Github Actions

--- agents/main.py
+++ agents/main.py
@@ -1,9 +1,15 @@
 from agent import *
 import argparse
+from dotenv import *
+import os
+
+load_dotenv()
+
+AGENT_ID = os.getenv("AGENT_ID")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Manage Bedrock agents.")
-    parser.add_argument("action", choices=["prepare", "delete", "list", "create", "update"], help="Action to perform (prepare, delete, or list agents).")
+    parser.add_argument("action", choices=["prepare", "delete", "list", "create", "update", "ciupdate"], help="Action to perform (prepare, delete, or list agents).")
     parser.add_argument("agent_id", nargs='?', help="The ID of the agent to prepare or delete. Not required for listing.")
     parser.add_argument("--name", help="The name of the agent to create.", required=False)
     parser.add_argument("--agentid", help="The ID of the agent to update.", required=False)
@@ -20,6 +26,8 @@ if __name__ == "__main__":
         if not args.name:
             parser.error("The --name argument is required for the create action.")
         create_agent(args.name)
+
+    # Manual CLI update
     elif args.action == "update":
         if not args.agentid:
             parser.error("The agent_id argument is required for the update action.")
@@ -29,3 +37,15 @@ if __name__ == "__main__":
         print(args.agentid)
         print(args.name)
         update_agent(args.agentid, args.name)
+
+    # This is meant for Github Actions
+    elif args.action == "ciupdate":
+        bedrock = boto3.client (
+            service_name='bedrock-agent', 
+            region_name='us-east-1',
+            aws_access_key_id=AWS_ID,
+            aws_secret_access_key=AWS_KEY
+        )
+        name = bedrock.get_agent(agentId=AGENT_ID).get('agent').get('agentName')
+        update_agent(AGENT_ID, name)
+
