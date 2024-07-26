0b6536fbd77ad5f42d289cd46d665430192dc588
ultralapse <136134023+ultralapse@users.noreply.github.com>
2024-07-26 16:02:36 -0400

    feat: switch to anthropic models for better performance and lower cost

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -1,5 +1,4 @@
 from aws_cdk import (
-    # Duration,
     Stack,
     aws_bedrock as bedrock,
     aws_iam as iam,
@@ -8,40 +7,12 @@ from aws_cdk import (
     aws_apigateway as apigateway,
     Duration,
     aws_dynamodb as dynamodb,
-    CfnOutput
 )
 from constructs import Construct
-import string
-import random
 import os
 
 from dotenv import load_dotenv
 load_dotenv()
-
-base_prompt_template='''System: A chat between a curious User and an artificial intelligence Bot. The Bot gives helpful, detailed, and polite answers to the User's questions. In this session, the model has access to external functionalities.
-To assist the user, you can reply to the user or invoke an action. Only invoke actions if relevant to the user request.
-$instruction$
-
-The following actions are available:$tools$
-Model Instructions:
-$model_instructions$
-$conversation_history$
-User: $question$
-$thought$ $bot_response$'''
-
-kb_template='''A chat between a curious User and an artificial intelligence Bot. The Bot gives helpful, detailed, and polite answers to the User's questions.
-
-In this session, the model has access to search results and a user's question, your job is to answer the user's question using only information from the search results.
-
-Model Instructions:
-- You should provide concise answer to simple questions when the answer is directly contained in search results, but when comes to yes/no question, provide some details.
-- In case the question requires multi-hop reasoning, you should find relevant information from search results and summarize the answer based on relevant information with logical reasoning.
-- If the search results do not contain information that can answer the question, please state that you could not find an exact answer to the question, and if search results are completely irrelevant, say that you could not find an exact answer, then summarize search results.
-- Remember to add a citation to the end of your response using markers like %[1]%, %[2]%, %[3]%, etc for the corresponding passage supports the response.
-- DO NOT USE INFORMATION THAT IS NOT IN SEARCH RESULTS!
-
-User: $query$ Bot:
-Resources: Search Results: $search_results$ Bot:'''
 
 class CdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
@@ -128,50 +99,18 @@ class CdkStack(Stack):
                         metadata_field="metadataField",
                         text_field="textField"
                     ),
-
-                    # the properties below are optional
                     namespace="namespace"
                 ),
             ),
-
-            # # the properties below are optional
-            # description="description",
-            # tags={
-            #     "tags_key": "tags"
-            # }
         )
 
         cfn_data_source = bedrock.CfnDataSource(self, "MyCfnDataSource",
             data_source_configuration=bedrock.CfnDataSource.DataSourceConfigurationProperty(
-                s3_configuration=bedrock.CfnDataSource.S3DataSourceConfigurationProperty(
-                    bucket_arn=bucket.bucket_arn,
-
-                    # the properties below are optional
-                    # bucket_owner_account_id="bucketOwnerAccountId",
-                    # inclusion_prefixes=["inclusionPrefixes"]
-                ),
+                s3_configuration=bedrock.CfnDataSource.S3DataSourceConfigurationProperty(bucket_arn=bucket.bucket_arn),
                 type="S3"
             ),
             knowledge_base_id=cfn_knowledge_base.attr_knowledge_base_id,
             name=f"source{construct_id}",
-
-            # # the properties below are optional
-            # data_deletion_policy="dataDeletionPolicy",
-            # description="description",
-            # server_side_encryption_configuration=bedrock.CfnDataSource.ServerSideEncryptionConfigurationProperty(
-            #     kms_key_arn="kmsKeyArn"
-            # ),
-            # vector_ingestion_configuration=bedrock.CfnDataSource.VectorIngestionConfigurationProperty(
-            #     chunking_configuration=bedrock.CfnDataSource.ChunkingConfigurationProperty(
-            #         chunking_strategy="chunkingStrategy",
-
-            #         # the properties below are optional
-            #         fixed_size_chunking_configuration=bedrock.CfnDataSource.FixedSizeChunkingConfigurationProperty(
-            #             max_tokens=123,
-            #             overlap_percentage=123
-            #         )
-            #     )
-            # )
         )
 
         cfn_role = iam.CfnRole(self, "AmazonBedrockAgentRole",
@@ -193,14 +132,9 @@ class CdkStack(Stack):
                         "Statement": [
                             {
                                 "Effect": "Allow",
-                                "Action": "bedrock:InvokeModel",
+                                "Action": ["bedrock:InvokeModel", "bedrock:Retrieve"],
                                 "Resource": "*",
                             },
-                            {
-                                "Effect": "Allow",
-                                "Action": "bedrock:Retrieve",
-                                "Resource": "*",
-                            }
                         ],
                     },
                 }
@@ -213,54 +147,17 @@ class CdkStack(Stack):
             description='Production',
             agent_resource_role_arn= cfn_role.attr_arn,
             auto_prepare=True,
-            foundation_model="amazon.titan-text-premier-v1:0",
-            instruction="You are a chatbot for the University of Massachusetts Lowell. Your goal is to answer questions to the best of your ability. Please ask the user to clarify if necessary",
+            foundation_model="anthropic.claude-3-haiku-20240307-v1:0",
+            instruction="Your identity is Rowdy the Riverhawk, a chatbot for the University of Massachusetts Lowell (Also known as UML). You will be answering questions about the university.",
             knowledge_bases=[bedrock.CfnAgent.AgentKnowledgeBaseProperty(
-                description="Pull information from here when user asks about the University of Massachusetts Lowell",
+                description="Contains information about the university",
                 knowledge_base_id=cfn_knowledge_base.attr_knowledge_base_id,
-
-                # the properties below are optional
-                # knowledge_base_state="knowledgeBaseState"
             )],
-            prompt_override_configuration=bedrock.CfnAgent.PromptOverrideConfigurationProperty(
-                prompt_configurations=[
-                    bedrock.CfnAgent.PromptConfigurationProperty(
-                        base_prompt_template=base_prompt_template,
-                        inference_configuration=bedrock.CfnAgent.InferenceConfigurationProperty(
-                            maximum_length=2048,
-                            temperature=0,
-                            top_p=0.1,
-                        ),
-                        prompt_creation_mode="OVERRIDDEN",
-                        prompt_type="ORCHESTRATION"
-                    ),
-
-                    bedrock.CfnAgent.PromptConfigurationProperty(
-                        base_prompt_template=kb_template,
-                        inference_configuration=bedrock.CfnAgent.InferenceConfigurationProperty(
-                            maximum_length=2048,
-                            temperature=0,
-                            top_p=0.1,
-                        ),
-                        prompt_creation_mode="OVERRIDDEN",
-                        prompt_type="KNOWLEDGE_BASE_RESPONSE_GENERATION"
-                    )
-                ],
-            ),
         )
 
         cfn_agent_alias = bedrock.CfnAgentAlias(self, f"MyCfnAgentAlias{construct_id}",
             agent_alias_name=f"Production{construct_id}",
             agent_id=cfn_agent.attr_agent_id,
-
-            # # the properties below are optional
-            # description="description",
-            # routing_configuration=[bedrock.CfnAgentAlias.AgentAliasRoutingConfigurationListItemProperty(
-            #     agent_version="agentVersion"
-            # )],
-            # tags={
-            #     "tags_key": "tags"
-            # }
         )
 
         table = dynamodb.TableV2(self, f"Table{construct_id}", partition_key=dynamodb.Attribute(name="userID", type=dynamodb.AttributeType.STRING))
@@ -288,7 +185,3 @@ class CdkStack(Stack):
             handler=dockerFunc,
             proxy=True,
         )
-
-        
-
-        # CfnOutput(self, "Knowledge Base ID: ", value=cfn_knowledge_base.attr_knowledge_base_id)
